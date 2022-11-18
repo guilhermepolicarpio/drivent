c46df8bf3c5a69b0e7da25c1ade0300266c4250b
Guilherme <guilhermepolicarpio@gmail.com>
2022-11-18 09:17:02 -0300

    first commit

---
+++ src/controllers/tickets-controller.ts
@@ -0,0 +1,3 @@
+import { Response } from "express";
+import httpStatus from "http-status";
+import { AuthenticatedRequest } from "@/middlewares";
---
+++ src/routers/tickets-router.ts
@@ -0,0 +1,3 @@
+import { Router } from "express";
+import { authenticateToken } from "@/middlewares";
+import (getTickets, getTicketTypes);
